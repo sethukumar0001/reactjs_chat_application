bc984dc420648b8501c3e7920e6b7e98d92d2eea
sethukumar0001 <boylooksrock@gmail.com>
2019-06-23 15:36:53 +0530

    first_commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAaODb7bCoZPvV4gdVyG_sV_Lc1_GuVdwg",
+    authDomain: "react-intro-37cd1.firebaseapp.com",
+    databaseURL: "https://react-intro-37cd1.firebaseio.com",
+    projectId: "react-intro-37cd1",
+    storageBucket: "react-intro-37cd1.appspot.com",
+    messagingSenderId: "181293593583"
+  };
